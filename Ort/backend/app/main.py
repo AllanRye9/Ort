3a09954945bc8205b513754c79ccbe14649cc149
Oryema Allan <34004636+AllanRye9@users.noreply.github.com>
2025-12-27 22:01:06 +0000

    files added

--- Ort/backend/app/main.py
+++ Ort/backend/app/main.py
@@ -1,12 +1,13 @@
 from .core.config import config
 from .core.security import security
 from .models.models import create_tables
-# from .models.seed import seed_data
+from .api.v1.api import app, home
 
 def main():
     config()
     security()
     create_tables()
+    home()
 
 if __name__ == "__main__":
  main()
