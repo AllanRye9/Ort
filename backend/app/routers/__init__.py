bffe0056c9846df08b686814225d072810ba2d94
Oryema Allan <34004636+AllanRye9@users.noreply.github.com>
2026-02-09 23:52:16 +0400

    Revert "[WIP] Add FastAPI backend structure for real estate software"

--- backend/app/routers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth import router as auth_router
-from .users import router as users_router
-from .properties import router as properties_router
-from .transactions import router as transactions_router
-
-__all__ = ["auth_router", "users_router", "properties_router", "transactions_router"]
