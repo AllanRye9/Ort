e1f7b86fa47b2b05327b486ae5a59e71136b8c63
ory9 <163534977+ory9@users.noreply.github.com>
2026-08-27 22:59:45 +0000

    2026-08-28 02:59:45

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+git pull orgin main
+npm install
 cd frontend && rm package-lock.json node_module && npm install 
 npm run build && cd ../backend && rm package-lock.json node_module && npm install && npm build ; cd ..
 git add .; git commit -m"$(TZ='Asia/Dubai' date +'%Y-%m-%d %H:%M:%S')" && git push
